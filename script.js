24a8ad41711e2d4b25b34e2814065eb6e64dcb54
filiptraum <offender54@ukr.net>
2020-10-15 13:40:03 +0300

    second commit
    Новие секции. Проверка JS, а именно скрипт changeBtn

--- script.js
+++ script.js
@@ -45,4 +45,80 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   bgChanging('.home');
 
+  // twoFonChanging
+  function twoFonChanging(bgblockSelector) {
+
+    const bgblock = document.querySelector(bgblockSelector);
+
+    setTimeout(() => {
+      bgblock.classList.add('about__pictures-fon1');
+    }, 3500);
+
+    setTimeout(() => {
+      bgblock.classList.remove('about__pictures-fon1');
+      bgblock.classList.add('about__pictures-fon2');
+    }, 7000);
+
+    setTimeout(() => {
+      bgblock.classList.remove('about__pictures-fon2');
+      bgblock.classList.add('about__pictures-fon3');
+    }, 10500);
+
+    setTimeout(() => {
+      bgblock.classList.remove('about__pictures-fon3');
+      return twoFonChanging(".about__pictures");
+    }, 14000);
+  }
+  twoFonChanging(".about__pictures");
+
 });
+
+// changeBtn
+const changeBtn = (itemsSelector, btnSelector, contentSelector, classHidden, activeBtn, classAnim) => {
+
+  const itemsBlock = document.querySelector(itemsSelector);
+  const btn = document.querySelectorAll(btnSelector);
+  const content = document.querySelectorAll(contentSelector);
+
+  function hideContent() {
+    content.forEach(item => {
+      item.classList.add(classHidden);
+      item.classList.remove(classAnim);
+    });
+
+    btn.forEach(item => {
+      item.classList.remove(activeBtn);
+    });
+  }
+
+  function showContent(i = 0) {
+    btn[i].classList.add(activeBtn);
+    let thisContent = document.querySelectorAll(btn[i].classList[0]);
+    thisContent.forEach((item) => {
+      item.classList.remove(classHidden);
+      item.classList.remove(classAnim);
+      setTimeout(() => {
+        item.classList.add(classAnim);
+      }, 0.1);
+    });
+  }
+
+  hideContent();
+  showContent();
+
+  itemsBlock.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains(btnSelector.replace(/\./, ""))) {
+      btn.forEach((item, i) => {
+        if (target == item) {
+          hideContent();
+          showContent(i);
+        }
+      });
+    }
+  });
+};
+
+changeBtn(".works__list", ".works__button", ".works__box", "hidden_image", "active", "works_box-anim");
+
+// playVideo
